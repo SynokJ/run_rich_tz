3de5095a1ee409ea169ecb2dd0fb969354be0cb9
Rustem Maldybekov <mrustem501@gmail.com>
2025-03-29 18:53:35 +0600

    Created basic player interaction

---
+++ run_rich_tz/Assets/Scripts/Player/Interaction/Playerinteraction.cs
@@ -0,0 +1,13 @@
+namespace Player.Interaction
+{
+    using Coins;
+    using UnityEngine;
+
+    public class Playerinteraction : MonoBehaviour
+    {
+        private void OnTriggerEnter(Collider other)
+        {
+            // TODO 
+        }
+    }
+}
